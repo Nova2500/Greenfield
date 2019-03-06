0afbcdabfae98a29ca71c1a1607dae724d6e2d45
Kevin Murphy <kmurphy@kmurphy.taos.local>
2019-03-06 11:36:37 -0800

    :art: Revised Tags module typo

--- tags/main.tf
+++ tags/main.tf
@@ -11,8 +11,7 @@ locals {
   "app" = "${lower(join(var.delimiter,var.application))}",
   "env_name" = "${lower(join(var.delimiter,var.env_name))}",
   "machine_role" = "${lower(join(var.delimiter,var.machine_role))}",
-  "managed_by" = "${lower(join(var.delimiter,var.managed_by))}"
-
+  "managed_by" = "${lower(join(var.delimiter,var.managed_by))}",
   "attributes" = "${lower(join(var.delimiter, var.attributes))}",
   "namespace" = "${lower(join(var.delimiter,var.namespace))}",
   "add_tags"= "${lower(join(var.delimiter,var.add_tags))}"
